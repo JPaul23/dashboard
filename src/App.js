21f73dd37ccb596a3e4e4f4e5756d4fdd1624076
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-06-14 21:23:50 +0200

    Notification & Profile

--- src/App.js
+++ src/App.js
@@ -77,6 +77,7 @@ const App = () => {
                 <Route path='/stacked' element={<Stacked />} />
               </Routes>
             </div>
+            <Footer />
           </div>
         </div>
       </BrowserRouter>
